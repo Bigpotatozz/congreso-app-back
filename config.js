bd8461fd972be8546c1f89238adc877a8c1ebd37
oscar <oscarmancilla227@gmail.com>
2025-11-06 12:10:20 -0600

    fix vercel

--- config.js
+++ config.js
@@ -1,33 +1,25 @@
-const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
+const { Pool } = require("pg");
 //Accede a DOTENV
 dotenv.config();
 
 //Crea un pool para la interaccion con base de datos
 //Usa las variables de entorno
-const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10,
-  idleTimeout: 60000,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-//Conecta el pool
-pool
-  .getConnection()
-  .then((connection) => {
-    console.log("Conectado a la base de datos");
-  })
-  .catch((err) => {
+pool.connect((err, res) => {
+  if (err) {
+    // SI HAY UN ERROR DURANTE LA CONEXIÓN, IMPRIME EL ERROR EN LA CONSOLA.
     console.log(err);
-  });
+  } else {
+    // SI LA CONEXIÓN ES EXITOSA, IMPRIME UN MENSAJE INDICANDO LA CONEXIÓN EXITOSA.
+    console.log("Conexión exitosa a la Base de Datos");
+  }
+});
 
 module.exports = { pool };
