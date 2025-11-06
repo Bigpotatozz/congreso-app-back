ae0d0f430f1db47dbc5ea97b2b3b46843984d109
oscar <oscarmancilla227@gmail.com>
2025-11-06 13:03:51 -0600

    ya porfa we

--- Controllers/usuarioController.js
+++ Controllers/usuarioController.js
@@ -23,9 +23,9 @@ const registrarParticipante = async (req, res) => {
 
   try {
     const queryRegistro = `INSERT INTO participante(nombre, apellidoM, apellidoP, email, twitter, ocupacion, foto)
-    VALUES(?,?,?,?,?,?,?)`;
+    VALUES($1,$2,$3,$4,$5,$6,$7)`;
 
-    const participanteR = pool.query(queryRegistro, [
+    const participanteR = await pool.query(queryRegistro, [
       participante.nombre,
       participante.apellidoM,
       participante.apellidoP,
@@ -49,7 +49,7 @@ const registrarParticipante = async (req, res) => {
 const obtenerParticipante = async (req, res) => {
   const { id } = req.params;
   try {
-    const queryParticipante = `SELECT * FROM participante where id_participante = ?`;
+    const queryParticipante = `SELECT * FROM participante where id_participante = $1`;
 
     const participante = await pool.query(queryParticipante, [id]);
 
@@ -73,7 +73,7 @@ const obtenerParticipante = async (req, res) => {
 const obtenerParticipantePorNombre = async (req, res) => {
   const { query } = req.query;
   try {
-    const queryParticipante = `SELECT * FROM participante where nombre like ?`;
+    const queryParticipante = `SELECT * FROM participante where nombre like $1`;
     const participante = await pool.query(queryParticipante, [query]);
 
     if (participante[0].length < 1) {
